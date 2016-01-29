fa291a03ad9c44b7ada82154067fbc0a88317ad3
Bona <bjorn.fyrvall@gmail.com>
2016-01-30 00:07:01 +0100

    Sooo tired roght now. Some progress though.

---
+++ Assets/Scripts/PlayerTurnManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerTurnManager : NetworkBehaviour
+{
+    public PlayerScript[] PlayerScripts;
+    public PlayerScript CurrentPlayer;
+
+    [SyncVar(hook = "SetTimeInterval")]
+    public float TimerInterval;
+
+    [SyncVar(hook = "SetTimerHook")]
+    public float CurrentTimer;
+
+    [SyncVar(hook = "SetIsActive")]
+    public bool IsActive;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        OnUpdate();
+	}
+
+    public void OnUpdate()
+    {
+        CurrentTimer -= Time.deltaTime;
+        if(CurrentTimer <= 0) {
+            if (isServer) {
+                OnTimerExpires();
+            }
+        }
+
+        var scaleFactor = CurrentTimer / TimerInterval;
+        transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+    }
+
+    public void OnTimerExpires()
+    {
+        Debug.Log("Server timer expires");
+        ResetTimer();
+    }
+
+    public void ResetTimer()
+    {
+        CurrentTimer = TimerInterval;
+    }
+
+    public void SetTimeInterval(float interval)
+    {
+        TimerInterval = interval;
+    }
+
+    public void SetTimerHook(float timer)
+    {
+        CurrentTimer = timer;
+    }
+
+    public void SetIsActivePlayer(bool isActive)
+    {
+
+    }
+}
