b8b23e255d3b0ab3984a24ee75abcd276b940c23
Tove <tove.brant@gmail.com>
2016-01-29 23:53:06 +0200

    Spell, Spelllist

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+[CreateAssetMenu]
+[System.Serializable]
+public class Spell : ScriptableObject{
+
+    public List<Rune> Runes;
+    public string SpellName;
+
+
+}
---
+++ Assets/Scripts/SpellList.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class SpellList : MonoBehaviour {
+
+    public List<Spell> spells;
+  
+
+    public void CheckWhichSpell(List<Rune> runes)
+    {
+        Spell rightSpell = null;
+        foreach(Spell sp in spells)
+        {
+            if(runes.Count == sp.Runes.Count)
+            {
+                for (int i = 0; i < runes.Count; i++){
+                    if(runes[i] == sp.Runes[i])
+                    {
+                        rightSpell = sp;
+                    }
+                    else
+                    {
+                        rightSpell = null;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(rightSpell != null)
+        {
+            Debug.Log("Spell casted: " + rightSpell.SpellName);
+        }
+        else
+        {
+            Debug.Log("Fail");
+        }
+    }
+
+}
