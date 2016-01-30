0f352b3beb904a0cd982d6d9dfb4c96babbfc67c
theanaka <theanaka@gmail.com>
2016-01-30 16:12:53 +0100

    Soundfix

--- Assets/Scripts/RuneTouch.cs
+++ Assets/Scripts/RuneTouch.cs
@@ -51,5 +51,6 @@ public class RuneTouch : Manager<RuneTouch> {
         ClearRunes();
 
 		SoundScript.Instance.LetGo ();
+		SoundScript.Instance.PlaySound (SoundScript.Instance.spellcast);
     }
 }
