89bdcffea858646d603d66be49ec67de641015d0
Bona <bjorn.fyrvall@gmail.com>
2016-01-30 13:03:18 +0100

    Added stuff.

---
+++ Assets/Scripts/SinglePlayerTouch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinglePlayerTouch : MonoBehaviour {
+
+    public TouchInput TouchInput;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(TouchInput == null) {
+            TouchInput = GameObject.FindObjectOfType<TouchInput>();
+        }
+
+        TouchInput.UpdateInput(true);
+	}
+}
