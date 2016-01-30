652831569f006efb374b05701aed6fea1474f873
Bona <bjorn.fyrvall@gmail.com>
2016-01-30 19:39:59 +0100

    I shall push it! Yes!!!

---
+++ Assets/Scripts/Hotseat/HotseatBoard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotseatBoard : MonoBehaviour {
+
+    public HotseatPlayer Owner;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Enable()
+    {
+        foreach (var collider in GetComponentsInChildren<Collider2D>()) {
+            collider.enabled = true;
+        }
+    }
+
+    public void Disable()
+    {
+        foreach(var collider in GetComponentsInChildren<Collider2D>()) {
+            collider.enabled = false;
+        }
+    }
+}
