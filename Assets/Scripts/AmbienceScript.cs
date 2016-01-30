be2eb826a7238ac6783aedf7b42da618eb066ddc
theanaka <theanaka@gmail.com>
2016-01-30 15:43:26 +0100

    Soundscript etc

---
+++ Assets/Scripts/AmbienceScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbienceScript : Manager <AmbienceScript>{
+
+	public AudioSource ambience;
+
+	// Use this for initialization
+	void Start () {
+		ambience = GetComponent<AudioSource>();
+		ambience.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+		
+}
+
