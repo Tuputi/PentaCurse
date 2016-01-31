53dffa2f7587d932ddc6bd3a942e17c98195ec45
Bona <bjorn.fyrvall@gmail.com>
2016-01-31 12:34:34 +0100

    Working timer.

---
+++ Assets/Scripts/Hotseat/HotseatTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Slider))]
+public class HotseatTimer : MonoBehaviour {
+
+    public Slider Slider;
+	// Use this for initialization
+	void Start ()
+    {
+        Slider = GetComponent<Slider>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        var timerFactor = 1.0f;
+         if (HotseatManager.Instance.CurrentGameState == HotSeatGameState.Countdown) {
+            timerFactor = HotseatManager.Instance.CurrentTimerValue / HotseatManager.Instance.CountdownTimer;
+        } else if (HotseatManager.Instance.CurrentGameState == HotSeatGameState.Playing) {
+            timerFactor = HotseatManager.Instance.CurrentTimerValue / HotseatManager.Instance.TurnTimerValue;
+        }
+
+        Slider.value = timerFactor;
+	}
+}
